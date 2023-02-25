62097214ce7429647769e7b0a1f81b95f32a9dab
hamzahouri <hamzahouri@gmail.com>
2023-02-25 03:39:14 +0100

    adding swagger

--- src/main/java/org/houri/QuickPoll/QuickPollApplication.java
+++ src/main/java/org/houri/QuickPoll/QuickPollApplication.java
@@ -2,8 +2,10 @@ package org.houri.QuickPoll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class QuickPollApplication {
 
 	public static void main(String[] args) {
